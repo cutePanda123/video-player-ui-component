9530e5f8ee01369c84bd47ff8f0f2abc8246ecd8
Peter Li <peterli1234@126.com>
2020-11-17 19:42:02 -0800

    setup html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/main.js',
@@ -14,5 +15,10 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
     mode: "development"
 };
